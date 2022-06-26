9f2811da3444304c66b640f646bcfced52d0ebb1
fqurishi <faislqurishi@gmail.com>
2022-06-26 01:48:59 -0700

    Add project files.

---
+++ AirScheduler/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AirScheduler.Startup))]
+namespace AirScheduler
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
