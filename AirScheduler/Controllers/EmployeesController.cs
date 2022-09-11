6da0d4a5f414535bcf5c8bf0e900ce857c1e3eea
fqurishi <faislqurishi@gmail.com>
2022-09-11 09:21:11 -0700

    adding in routes

---
+++ AirScheduler/Controllers/EmployeesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AirScheduler.Models;
+
+namespace AirScheduler.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: Employee/Random
+        public ViewResult Random()
+        {
+            var employee = new Employee() { Name = "Shrek" };
+            
+            return View(employee);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id= " + id);
+        }
+
+        // employees
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrEmpty(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+        [Route("employees/Employer/{eid}")]
+        public ActionResult ByEmployer(int eid)
+        {
+            return Content(eid + "");
+        }
+    }
+}
